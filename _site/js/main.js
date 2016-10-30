ca4d338ba38c7dd879c203138a55ca38993cf63d
Michael Møller <mim@planday.com>
2016-10-30 22:03:36 +0100

    Trying again

---
+++ _site/js/main.js
@@ -0,0 +1,21 @@
+jQuery(document).ready(function($) {
+    
+    setTimeout(function(){
+        var videoheight = $('video').outerHeight();
+        var h1_top = (videoheight / 2);
+        console.log(videoheight);
+         $('.splash').css('height', videoheight);
+         $('.splash').css('top', h1_top);
+         $('.splash .headline').fadeIn(3000);
+    }, 1000);
+    
+    // console.log(h1_top);
+    
+    $(window).resize(function() {
+        videoheight = $('video').height();
+        h1_top = (videoheight / 2);
+        $('.splash').css('top', h1_top);
+        $('.splash').css('height', videoheight);
+    });
+
+});
